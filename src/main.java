aa16cc8252241e0bc01f3a5285d8e8d02bd124c2
sylvain-grousset <68558176+sylvain-grousset@users.noreply.github.com>
2022-07-31 20:43:07 +0200

    Update : Main class with code to start download/unzip/parser/storeToDB

--- src/main.java
+++ src/main.java
@@ -1,10 +1,60 @@
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, SQLException {
+    	LocalDate now = LocalDate.now();
+    	LocalDate prevDate = now.minusDays(1);
+    	
+    	String formattedDate = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+    	String formattedDate2 = prevDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+    	
+     	if (checkFile(formattedDate2) == true ){
+     		UnzipUtility unzipper = new UnzipUtility();
+            try {
+                unzipper.unzip(formattedDate2+".zip", "histoFichiers/");
+                XMLReader xr = XMLReaderFactory.createXMLReader();
+                Sax sax = new Sax();
+                xr.setContentHandler(sax);
+                xr.parse("histoFichiers/PrixCarburants_quotidien_"+formattedDate2+".xml");
+            } catch (Exception ex) {
+                // some errors occurred
+                ex.printStackTrace();
+            }
+     	}
+    }
+    
+    private static boolean checkFile(String date) throws IOException {
+    	URL website = new URL("https://donnees.roulez-eco.fr/opendata/jour/"+date);
+     	ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+     	FileOutputStream fos = new FileOutputStream(date+".zip");
+     	fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+     	
+     	HttpURLConnection conn = (HttpURLConnection)website.openConnection();
+     	String contentType = conn.getContentType();
+     	
+     	if(contentType.compareTo("application/zip") == 0) {
+     		return true;
+     	}else {
+     		return false;
+     	}
+    }
 }
